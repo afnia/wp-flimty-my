8036258c5b857a50bb74a5ab255b2ffc1b9f2cc9
Riza Masykur <riza@ikamai.com>
2025-04-15 16:45:44 +0700

    ok

--- local_build.sh
+++ local_build.sh
@@ -2,16 +2,9 @@
 
 
 
-# IMG="registry.gitlab.com/a8551/flimty/flimty-malaysia/wp-flimty-my:lp"
-# docker build -f DockerfileLp -t $IMG .
-# docker push $IMG
-# rsync -avze ssh ./docker-compose-lp.yml root@46.250.233.121:/home/wordpress/lp.flimty.my/docker-compose.yml
-# ssh root@46.250.233.121 'cd /home/wordpress/lp.flimty.my;docker-compose pull;docker-compose up -d --build'
-
-
-
-IMG="registry.gitlab.com/a8551/flimty/flimty-malaysia/wp-flimty-my:prod"
+IMG="registry.gitlab.com/aimi-new/wordpress/caradietmudah.com"
 docker build -t $IMG .
 docker push $IMG
-rsync -avze ssh ./docker-compose-prod.yml root@46.250.233.121:/home/wordpress/malaysia.flimty.co/docker-compose.yml
-ssh root@46.250.233.121 'cd /home/wordpress/malaysia.flimty.co;docker-compose pull;docker-compose up -d --build'
+
+# rsync -avze ssh ./docker-compose-prod.yml root@46.250.233.121:/home/wordpress/malaysia.flimty.co/docker-compose.yml
+# ssh root@46.250.233.121 'cd /home/wordpress/malaysia.flimty.co;docker-compose pull;docker-compose up -d --build'
